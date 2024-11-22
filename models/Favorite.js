8cbe9841f0f4e59e67f8db0c94f280f9bde98752
mmustafa56 <muhammadmustafapak71@gmail.com>
2024-11-22 21:06:08 +0400

    add timestamps to favorite model

--- models/Favorite.js
+++ models/Favorite.js
@@ -1,11 +1,29 @@
 const mongoose = require("mongoose");
 
-
-const favoriteSchema  = new mongoose.Schema({
-    _id: mongoose.Types.ObjectId.toString(),
-    moviesName: {
-        types: String,
-        default:""
+const favoriteSchema = new mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId
     },
+    movieName: {
+        type: String,
+        required: true,
+        trim: true, 
+    },
+    movieId: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+    genre: {
+        type: String, 
+        default: "Unknown",
+    },
+}, {
+    timestamps: true, 
+});
 
-})
+module.exports = mongoose.model("Favorite", favoriteSchema);
